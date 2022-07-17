f9c7af396723ff870f22e8f10b42ca110b64585c
Jeff Trempe <trempe.jeff@gmail.com>
2022-07-16 23:24:42 -0400

    First save after adding the game class

---
+++ gamelogic/game.ts
@@ -0,0 +1,10 @@
+class Game {
+  public constructor(letters: string[]) {
+    this.players = [];
+    this.letters = letters;
+  }
+
+  public pickLetters(quantity: number): string[] {
+    this.letters.
+  }
+}
