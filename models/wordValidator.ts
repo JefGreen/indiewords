885d70d5957eafc88ab2aaf0cb2fc7f382fee2b9
Jeff Trempe <trempe.jeff@gmail.com>
2022-07-21 22:30:25 -0400

    Added theme

--- models/wordValidator.ts
+++ models/wordValidator.ts
@@ -1,9 +1,35 @@
+type Word = string
+type Description = string
+type Dictionary = Record<Word, Description>
+
+
 class WordValidator {
-  words: Record<string, number> = {
+  words: Dictionary = {
     "word": 12,
   }
 
-  validate(word: string): Boolean {
+  theme: Dictionary = {}
+
+  constructor(theme?: Dictionary) {
+    if (theme !== undefined) {
+      this.theme = theme;
+    }
+  }
+
+  isWordValid(word: string): Boolean {
+    if (this.theme)
     return !!this.words[word]
   }
+
+
+
+
+
+
 }
+
+
+const validator = new WordValidator()
+
+console.log(validator.isWordValid("word"))
+console.log(validator.isWordValid("wtord"))
