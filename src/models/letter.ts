ac5e2dce527030a374304108b3f14e8a4cf026c9
Jeff Trempe <trempe.jeff@gmail.com>
2022-08-10 23:57:03 -0400

    Reformating letter class

--- src/models/letter.ts
+++ src/models/letter.ts
@@ -1,16 +1,9 @@
 export class Letter {
-  constructor(
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    value: string
-  ) {
+  constructor(x: number, y: number, width: number, height: number, value: string) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
-
     this.value = value;
   }
 
