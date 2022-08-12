c8e405655fc41cb82888f85b47206b6495aaa889
Jeff Trempe <trempe.jeff@gmail.com>
2022-08-12 00:02:25 -0400

    Trying to type the lettersPool

--- src/stores/gamestore.ts
+++ src/stores/gamestore.ts
@@ -6,8 +6,7 @@ type Letters = Array<string>;
 type Words = Array<Word>;
 type Dictionary = Set<string>;
 
-const lettersPool: Letters = []
-  .concat(Array(13).fill("a"))
+const lettersPool: Letters = Array.prototype.concat(Array(13).fill("a"))
   .concat(Array(3).fill("b"))
   .concat(Array(3).fill("c"))
   .concat(Array(6).fill("d"))
