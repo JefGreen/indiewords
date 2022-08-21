fd7f1646677b47d7714c73c1d06cc9ebfd33bfc4
Jeff Trempe <trempe.jeff@gmail.com>
2022-08-20 23:17:19 -0400

    Removed hello world and cleaned up a little bit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-import Board from "./components/Board.vue";
+import Board from './components/Board.vue';
 // import WordDictionary from "./models/wordValidator"
 </script>
 
 <template>
   <div>
-    <!-- TODO: start using pixi js -->
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
   <Board />
 </template>
 
